b2946d856a3e3208e4565490afadde80d95f8412
Kat Landreth <katlandreth@gmail.com>
2016-05-26 17:15:35 -0500

    fixes class name in generator to match filename

--- lib/generators/booqmark/booqmark_view_generator.rb
+++ lib/generators/booqmark/booqmark_view_generator.rb
@@ -1,13 +1,13 @@
 module Booqmark
   module Generators
 
-    class BooqmarkPartialGenerator < Rails::Generators::Base
+    class BooqmarkViewGenerator < Rails::Generators::Base
       source_root File.expand_path("../../../..", _FILE_)
 
       def create_booqmark_partial
         directory "app/views/booqmark/", "app/views/booqmark/_booqmark_toolbar.html.erb"
       end
     end
-    
+
   end
 end
