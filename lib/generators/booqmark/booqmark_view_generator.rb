9e1431ec7fcb6b88393e7fce82c30f13cd97f1de
Kat Landreth <katlandreth@gmail.com>
2016-05-26 17:22:14 -0500

    fixes missing underscores around __FILE__ causing undefined variable error

--- lib/generators/booqmark/booqmark_view_generator.rb
+++ lib/generators/booqmark/booqmark_view_generator.rb
@@ -2,7 +2,7 @@ module Booqmark
   module Generators
 
     class BooqmarkViewGenerator < Rails::Generators::Base
-      source_root File.expand_path("../../../..", _FILE_)
+      source_root File.expand_path("../../../..", __FILE__)
 
       def create_booqmark_partial
         directory "app/views/booqmark/", "app/views/booqmark/_booqmark_toolbar.html.erb"
