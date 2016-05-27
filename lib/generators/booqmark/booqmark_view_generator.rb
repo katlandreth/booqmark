9b489f0f50be5abd70fc2d9e1faffb9b49d58bbb
Kat Landreth <katlandreth@gmail.com>
2016-05-27 09:02:59 -0500

    fix destination path in generator

--- lib/generators/booqmark/booqmark_view_generator.rb
+++ lib/generators/booqmark/booqmark_view_generator.rb
@@ -5,7 +5,7 @@ module Booqmark
       source_root File.expand_path("../../../..", __FILE__)
 
       def create_booqmark_partial
-        directory "app/views/booqmark/", "app/views/booqmark/_booqmark_toolbar.html.erb"
+        directory "app/views/booqmark/", "app/views/booqmark/"
       end
     end
 
