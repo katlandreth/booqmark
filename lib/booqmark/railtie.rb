5e2a30024a4d0c20845d801ee1e8cb2f62179ff4
Kat Landreth <katlandreth@gmail.com>
2016-05-27 14:21:07 -0500

    bump version

--- lib/booqmark/railtie.rb
+++ lib/booqmark/railtie.rb
@@ -1,7 +1,7 @@
 module Booqmark
   class Railtie < Rails::Railtie
     initializer "booqmark.view_helper" do
-      ActiveSupport.on_load( :action_view){ include  Booqmark }
+      ActiveSupport.on_load( :action_view){ include  ViewHelper }
     end
   end
 end
