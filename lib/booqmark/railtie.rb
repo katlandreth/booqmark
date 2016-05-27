60aaa3e97bfde2d1df2c8995078e8e1034a488ca
Kat Landreth <katlandreth@gmail.com>
2016-05-27 14:20:43 -0500

    changes raltie to include ViewHelper not scoped to Booqmark

--- lib/booqmark/railtie.rb
+++ lib/booqmark/railtie.rb
@@ -1,7 +1,7 @@
 module Booqmark
   class Railtie < Rails::Railtie
     initializer "booqmark.view_helper" do
-      ActiveSupport.on_load( :action_view){ include Booqmark::ViewHelper }
+      ActiveSupport.on_load( :action_view){ include  Booqmark }
     end
   end
 end
